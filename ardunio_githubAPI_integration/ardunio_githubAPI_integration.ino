bc5e3b0fe70203b2f9176ca059a6f3ce555247a4
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2025-01-18 23:26:28 +0200

    Implement the logic for scanning for I2C device

--- ardunio_githubAPI_integration/ardunio_githubAPI_integration.ino
+++ ardunio_githubAPI_integration/ardunio_githubAPI_integration.ino
@@ -1,6 +1,16 @@
+#include <Wire.h>
+#include "i2c_device_scanner.h"
+
 void setup() {
   // put your setup code here, to run once:
+  Wire.begin();
+  Serial.begin(9600);
+  
+  byte i2cDeviceAddress = scanI2CDevices()
 
+  if(i2cDeviceAddress != 0xFF){
+    
+  }
 }
 
 void loop() {
---
+++ ardunio_githubAPI_integration/i2c_device_scanner.cpp
@@ -0,0 +1,38 @@
+#include "i2c_device_scanner.h"
+
+byte scanI2CDevices(){
+  byte error, address;
+
+  Serial.println("I2C devices scanning...");
+
+  for(address = 1; address < 127; address++){
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16){
+        Serial.print("0");
+      }
+
+      Serial.print(address,HEX);
+      Serial.println("  !");
+      return address;
+    }
+    else if (error==4)
+    {
+      Serial.print("Unknown error at address 0x");
+      
+      if (address < 16){
+        Serial.print("0");
+      }
+
+      Serial.println(address,HEX);
+    }
+    else{
+      Serial.println("No I2C devices found");
+      return 0xFF;
+    }
+  }
+}
