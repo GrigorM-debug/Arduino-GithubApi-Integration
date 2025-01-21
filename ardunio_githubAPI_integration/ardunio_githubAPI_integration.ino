13513f1cb74fd4cd425abec6dbc4eccd1f7f21ad
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2025-01-21 17:02:15 +0200

    Implemented logic for connecting the Arduino Uno R4 to Wifi

--- ardunio_githubAPI_integration/ardunio_githubAPI_integration.ino
+++ ardunio_githubAPI_integration/ardunio_githubAPI_integration.ino
@@ -4,30 +4,74 @@
 #include "Arduino_LED_Matrix.h"
 #include "led_matrix_frames.h"
 
+#include "arduino_secrets.h"
+#include <WiFiS3.h>
+
 ArduinoLEDMatrix matrix;
+
+int status = WL_IDLE_STATUS; // The WiFi radio's status
 
 void setup() {
   // put your setup code here, to run once:
   Wire.begin();
   Serial.begin(9600);
-  
-  //matrix.loadSequence(frames);
-  matrix.loadSequence(LEDMATRIX_ANIMATION_TETRIS_INTRO);
-  matrix.begin();  
-  matrix.play(true);
-
-  //matrix.renderBitmap(arduinoLogoframe, 8, 12);
 
   byte i2cDeviceAddress = scanI2CDevices();
 
   if(i2cDeviceAddress > 0){
     LiquidCrystal_I2C lcd(i2cDeviceAddress, 20, 4);
+    
+    //Think about moving it to a function
+    //
+    //Check for the Wifi module
+    if(WiFi.status() == WL_NO_MODULE){
+      Serial.println("Communication with Wifi Module failed.");
+      //don't contunue
+      while(true);
+    }
 
-    lcd.init();
-    lcd.backlight();
+    //Check the Firmware version
+    String firmwareVersion = WiFi.firmwareVersion();
+    if(firmwareVersion < WIFI_FIRMWARE_LATEST_VERSION){
+      Serial.println("Upgrade the firmware version");
+    }
 
-    lcd.setCursor(0, 0);
-    lcd.print(i2cDeviceAddress, HEX);
+    //Connecting to Wifi network
+    while(status != WL_CONNECTED){
+      Serial.println("Connecting to WPA SSID: ");
+      Serial.print(SECRET_SSID);
+      Serial.println("\n");
+
+      //Wifi search animation
+      matrix.loadSequence(LEDMATRIX_ANIMATION_WIFI_SEARCH);
+      matrix.begin();
+      matrix.play(true);
+
+      status = WiFi.begin(SECRET_SSID, SECRET_PASSWORD);
+
+      // wait 10 seconds for connection:
+      delay(10000);
+    }
+
+    if(status == WL_CONNECTED){
+      Serial.print("Succsselfully connected to WPA SSID: ");
+      Serial.println(SECRET_SSID);
+
+      matrix.loadFrame(LEDMATRIX_LIKE);
+      matrix.begin();
+      
+      lcd.init();
+      lcd.backlight();
+
+      lcd.setCursor(0, 0);          
+      lcd.print("Succssefully connected to WPA SSID: ");        
+      lcd.setCursor(0, 3);          
+      lcd.print(SECRET_SSID);
+    } 
+    else{
+      Serial.print("Failed to connect to WPA SSID: ");
+      Serial.println(SECRET_SSID);
+    }
   }
 }
 
