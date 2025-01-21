3b494099c5d1011c3ade0e8981fc1de616902909
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2025-01-21 18:32:08 +0200

    Made separated file for LED matrix animations

---
+++ ardunio_githubAPI_integration/arduino_LED_matrix_animations.cpp
@@ -0,0 +1,32 @@
+#include "arduino_LED_matrix_animations.h"
+//#include "led_matrix_frames.h"
+
+ArduinoLEDMatrix matrix;
+
+//Wifi searching animation
+void wifiSearchingAnimation(){
+  matrix.loadSequence(LEDMATRIX_ANIMATION_WIFI_SEARCH);
+  matrix.begin();
+  matrix.play(true);
+}
+
+//Connected to Wifi animation
+void wifiConnectedAnimation(){
+  matrix.loadFrame(LEDMATRIX_EMOJI_HAPPY);
+  matrix.begin();
+  delay(3000);
+}
+
+//Connection to Wifi failed animation
+void wifiNotConnectedAnimation(){
+  matrix.loadFrame(LEDMATRIX_EMOJI_SAD);
+  matrix.begin();
+  delay(3000);
+}
+
+//Default animation playing
+void defaultAnimation() {
+  matrix.loadSequence(LEDMATRIX_ANIMATION_TETRIS_INTRO);
+  matrix.begin();  
+  matrix.play(true);
+}
---
+++ ardunio_githubAPI_integration/arduino_LED_matrix_animations.h
@@ -0,0 +1,16 @@
+#ifndef ARDUINO_LED_MATRIX_ANIMATIONS_H
+#define ARDUINO_LED_MATRIX_ANIMATIONS_H
+
+#include <Arduino.h>
+#include "Arduino_LED_Matrix.h"
+//#include "led_matrix_frames.h"
+
+void wifiSearchingAnimation();
+
+void wifiConnectedAnimation();
+
+void wifiNotConnectedAnimation();
+
+void defaultAnimation();
+
+#endif
